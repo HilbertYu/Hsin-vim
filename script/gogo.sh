c12061b72e62037d2bdcf40f6a2b1522a1a032dd
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2020-04-07 00:35:45 +0800

    + gogo.sh

---
+++ script/gogo.sh
@@ -0,0 +1,10 @@
+./configure --with-features=huge \
+--enable-multibyte \
+--enable-rubyinterp=yes \
+--enable-python3interp=yes \
+--with-python-config-dir=/usr/local/bin/python3.7
+--enable-perlinterp=yes \
+--enable-luainterp=yes \
+--enable-cscope \
+--prefix=/Users/fresh/opt/
+
